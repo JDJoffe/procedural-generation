3d49380fcdd865785dbedc71b1d4967907c5b73c
josh <48034855+JDJoffe@users.noreply.github.com>
2019-09-04 15:07:04 +1000

    build textured quad

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MazeConstructor))]
+public class GameController : MonoBehaviour
+{
+    private MazeConstructor generator;
+
+    private void Start()
+    {
+        generator = GetComponent<MazeConstructor>();
+        generator.GenerateNewMaze(13, 15);
+    }
+}
---
+++ Assets/Scripts/MazeMeshGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeMeshGenerator 
+{
+    public float width;
+    public float height;
+
+    public MazeMeshGenerator()
+    {
+        width = 3.75f;
+        height = 3.5f;
+    }
+
+    public Mesh FromData(int[,] data)
+    {
+        Mesh maze = new Mesh();
+
+        List<Vector3> newVertices = new List<Vector3>();
+        List<Vector2> newUVs = new List<Vector2>();
+        List<int> newTriangles = new List<int>();
+
+        //corners of quad
+        Vector3 vert1 = new Vector3(-.5f, -.5f, 0);
+        Vector3 vert2 = new Vector3(-.5f, .5f, 0);
+        Vector3 vert3 = new Vector3(.5f, .5f, 0);
+        Vector3 vert4 = new Vector3(.5f, -.5f, 0);
+       
+        newVertices.Add(vert1);
+        newVertices.Add(vert2);
+        newVertices.Add(vert3);
+        newVertices.Add(vert4);
+
+        newUVs.Add(new Vector2(1, 0));
+        newUVs.Add(new Vector2(1, 1));
+        newUVs.Add(new Vector2(0, 1));
+        newUVs.Add(new Vector2(0, 0));
+
+        newTriangles.Add(2);
+        newTriangles.Add(1);
+        newTriangles.Add(0);
+
+        newTriangles.Add(3);
+        newTriangles.Add(2);
+        newTriangles.Add(0);
+
+        maze.vertices = newVertices.ToArray();
+        maze.uv = newUVs.ToArray();
+        maze.triangles = newTriangles.ToArray();
+
+        return maze;
+    }
+}
